d6f757ba57d92c6cd61060312eeafdf87ef3f079
Administrator <Administrator@XB-201708182132>
2018-07-02 17:50:29 +0800

    dubbo

--- src/com/hlcui/DemoApplication.java
+++ src/com/hlcui/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource("dubbo-server.xml")
+@ImportResource("dubbo-provider.xml")
 public class DemoApplication {
 
 	public static void main(String[] args) {
