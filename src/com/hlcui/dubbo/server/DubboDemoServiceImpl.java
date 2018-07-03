efe63663ab28b7b4d39368b11dd3106c1f7c04b0
Administrator <Administrator@XB-201708182132>
2018-07-03 17:36:40 +0800

    dubbo demo

--- src/com/hlcui/dubbo/server/DubboDemoServiceImpl.java
+++ src/com/hlcui/dubbo/server/DubboDemoServiceImpl.java
@@ -1,5 +1,8 @@
 package com.hlcui.dubbo.server;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class DubboDemoServiceImpl implements DubboDemoService{
 	public String sayHello(String name){
 		return "hello "+name;
