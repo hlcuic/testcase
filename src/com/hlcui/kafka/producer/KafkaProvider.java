daeee37ba4abb3bded76f65da0151543930e9fd9
Administrator <Administrator@XB-201708182132>
2018-07-04 13:18:23 +0800

    kafka provider demo

---
+++ src/com/hlcui/kafka/producer/KafkaProvider.java
@@ -0,0 +1,57 @@
+
+package com.hlcui.kafka.producer;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+public class KafkaProvider {
+
+	private final Producer<String, String> kafkaProducer;
+
+	public final static String TOPIC = "JAVA_TOPIC";
+
+	private KafkaProvider() {
+		kafkaProducer = createKafkaProducer();
+	}
+
+	private Producer<String, String> createKafkaProducer() {
+		Properties props = new Properties();
+		props.put("bootstrap.servers", "127.0.0.1:9092");
+		props.put("acks", "all");
+		props.put("retries", 0);
+		props.put("batch.size", 16384);
+		props.put("linger.ms", 1);
+		props.put("buffer.memory", 33554432);
+		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		Producer<String, String> kafkaProducer = new KafkaProducer<>(props);
+		return kafkaProducer;
+	}
+
+	private void produce() {
+		for (int i = 1; i < 1000; i++) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			String key = String.valueOf("key" + i);
+			String data = "hello kafka message:" + key;
+			kafkaProducer.send(new ProducerRecord<>(TOPIC, key, data), new Callback() {
+				public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+					System.out.println("send kafka message end!!!");
+				}
+			});
+			System.out.println(data);
+		}
+	}
+
+	public static void main(String[] args) {
+		new KafkaProvider().produce();;
+	}
+}
