14fed37b3655a41c3544ab3859037190c8094eaa
hello <hello@192.168.1.101>
2018-07-05 19:58:16 +0800

    code

--- src/com/hlcui/disruptor/TradeTransactionInDBHandler.java
+++ src/com/hlcui/disruptor/TradeTransactionInDBHandler.java
@@ -1,7 +1,5 @@
 package com.hlcui.disruptor;
 
-import java.util.UUID;  
-
 import com.lmax.disruptor.EventHandler;  
 import com.lmax.disruptor.WorkHandler;  
   
@@ -11,11 +9,9 @@ public class TradeTransactionInDBHandler implements EventHandler<TradeTransactio
     public void onEvent(TradeTransaction event) throws Exception {  
         this.onEvent(event);      
     }  
-    //消费者C2接到消息后的具体的业务 处理逻辑  
     @Override  
     public void onEvent(TradeTransaction event, long arg1, boolean arg2) throws Exception {  
-         //这里做具体的消费业务逻辑，即就是接到消息后的业务处理逻辑  
-       // event.setId(UUID.randomUUID().toString());//随机简单生成下ID 并且打印出来   
-        System.out.println("第二个消费者C2消费了消息 ========"+(i++));    
+       // event.setId(UUID.randomUUID().toString());
+        System.out.println("========"+(i++));    
     }  
 } 
